78d06bc08e66272ad0a0e39e8199a6d81d69fbdb
plazanetlima <plazanetlima@hotmail.com>
2022-10-23 09:30:07 -0500

    Ejercicio Resuelto

--- Clase 15-16/Ejercicio 6/scripts.js
+++ Clase 15-16/Ejercicio 6/scripts.js
@@ -1,5 +1,6 @@
 /* 
-6 - Escribe un programa que permita ir introduciendo una serie indeterminada de números mientras su suma no supere 50. Cuando esto ocurra, se debe mostrar el total acumulado y el contador de cuantos números se han introducido
+6 - Escribe un programa que permita ir introduciendo una serie indeterminada de números mientras su suma no supere 50. Cuando esto ocurra, se debe mostrar el total acumulado 
+y el contador de cuantos números se han introducido
 */
 
 let suma=0
