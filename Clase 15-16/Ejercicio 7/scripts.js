065825882bce71cbe3553a5404cbd191afad23b6
plazanetlima <plazanetlima@hotmail.com>
2022-10-23 09:31:20 -0500

    Ejercico Resuelto

--- Clase 15-16/Ejercicio 7/scripts.js
+++ Clase 15-16/Ejercicio 7/scripts.js
@@ -1,9 +1,10 @@
 /* 
-7 - Crea 3 arrays. El primero tendrá 5 números y el segundo se llamará pares y el tercero impares, ambos estarán vacíos. Después multiplica cada uno de los números del primer array por un número aleatorio entre 1 y 10, si el resultado es par guarda ese número en el array de pares y si es impar en el array de impares. Muestra por consola:
+7 - Crea 3 arrays. El primero tendrá 5 números y el segundo se llamará pares y el tercero impares, ambos estarán vacíos. Después multiplica cada uno de los números del primer array 
+    por un número aleatorio entre 1 y 10, si el resultado es par guarda ese número en el array de pares y si es impar en el array de impares. Muestra por consola:
     -la multiplicación que se produce junto con su resultado con el formato 2 x 3 = 6
     -el array de pares e impares
 */
-
+/*
 const numbers = [3,43,21,20,56]
 let pares = []
 let impares = []
@@ -22,3 +23,24 @@ for(const number of numbers){
 
 console.log(pares);
 console.log(impares);
+*/
+const producto = document.getElementById('producto');
+const resultPares = document.getElementById('resultPares');
+const resultImpares = document.getElementById('resultImpares');
+
+const arrayNumeros = [7,14,5,28,32];
+let arrayPares = [];
+let arrayImpares = [];
+let numAleatorio;
+let resultado;
+
+for(let i=0; i<arrayNumeros.length; i++){
+    numAleatorio = Math.round(Math.random()*10+1);
+    resultado = arrayNumeros[i]*numAleatorio;
+    producto.textContent = `${numAleatorio} X ${arrayNumeros[i]} = ${resultado}`;
+    if(resultado%2 == 0) arrayPares.push(resultado)
+    else arrayImpares.push(resultado);
+}
+resultPares.textContent = `Números pares: ${arrayPares}`;
+resultImpares.textContent = `Números impares: ${arrayImpares}`;
+
