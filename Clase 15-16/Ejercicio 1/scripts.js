879536f2d868086e2378b025c515b9577849f0dd
plazanetlima <plazanetlima@hotmail.com>
2022-10-22 21:23:15 -0500

    Ejercicio Resuelto

--- Clase 15-16/Ejercicio 1/scripts.js
+++ Clase 15-16/Ejercicio 1/scripts.js
@@ -3,7 +3,18 @@
 Realiza el ejercicio con prompt(mensaje) y haz uso de los template strings
 */
 
+/*
 let name = prompt('Introduce tu nombre')
 let age = parseInt(prompt('Introduce tu edad'))
 
 console.log(`Hola ${name} tienes ${age} años y el año que viene tendrás ${age+1} años`);
+*/
+
+const resultado = document.getElementById('resultado');
+
+let nombre = prompt('Escriba su nombre');
+let edad = prompt('Escriba su edad');
+
+//console.log(`Hola ${nombre}, tienes ${edad} años y el año que viene tendrás ${edad+1} años`);
+
+resultado.textContent = `Hola ${nombre}, tienes ${edad} años y el año que viene tendrás ${edad+1} años`;
