b95e30aa1c278ae54c2bc2da5ad96ee3be9c05b0
plazanetlima <plazanetlima@hotmail.com>
2022-10-23 09:28:21 -0500

    Ejercicio Resuelto

--- Clase 15-16/Ejercicio 4/scripts.js
+++ Clase 15-16/Ejercicio 4/scripts.js
@@ -2,7 +2,7 @@
 4 - Escribe un programa que pida un número entero mayor que 1 y que escriba si el número primo o no.
     Un número primo es aquel que solo es divisible por sí mismo y la unidad
 */
-
+/*
 let num = parseInt(prompt('Introduce un número'))
 let divisores = 0
 
@@ -19,3 +19,21 @@ else {
 }
 
 if(divisores==0) console.log(`${num} es primo`);
+*/
+
+let numero = parseInt(prompt('Ingrese el número mayor que 1 para evaluar si es PRIMO'));
+esPrimo = true;
+
+if(numero > 1){
+    for(let i=2; i<numero; i++){
+        if(numero % i == 0){
+            esPrimo = false;
+            break;
+        }
+    }
+}
+else
+    document.body.innerHTML = `${numero} no es un número válido`;
+
+if(esPrimo) document.body.innerHTML = `${numero} es un número PRIMO`
+else document.body.innerHTML = `${numero} NO es un número PRIMO`;
